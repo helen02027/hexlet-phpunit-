38c65db75a015d163c15ac4560728298660dd9b7
Elena Kilyanova <helen02027@gmail.com>
2025-02-27 17:19:47 +0300

    самостоятельная работа фикстура

--- tests/UtilsTest.php
+++ tests/UtilsTest.php
@@ -18,4 +18,17 @@
       $this->assertEquals('', reverseString(''));
       $this->assertEquals('olleh', reverseString('hello'));
     }
+    
+    public function testExtractLinks()
+    {
+      // HTML находится в файле withLinks.html в директории fixtures
+      // При чтении текстовых файлов, в конце может добавляться пустая строка
+      // Она удаляется с помощью метода `trim`, если нужно
+      // __DIR__ – директория, в которой находится данный файл с тестами
+      $html = file_get_contents(__DIR__ . "/../tests/fixtures/text.html");
+      // Теперь с HTML удобно работать и он не загромождает тесты.
+      $links = reverseString($html);
+      $this->assertEquals('>lmth epytcod!<', $links);
+    }
   }
+  
